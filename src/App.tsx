e0b7138b490604da696a18d6aa22a0f2d40e5e16
Joel Adewole <joeladewole3@gmail.com>
2022-10-18 19:36:24 +0100

    Created custom dashboard with refine

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
-import {
-  CssBaseline,
-  GlobalStyles,
-  ThemeProvider,
-  LightTheme,
-} from "@pankod/refine-mui";
+import { Refine } from "@pankod/refine-core";
+import dataProvider from "@pankod/refine-simple-rest";
+import routerProvider from "@pankod/refine-react-router-v6";
 import Dashboard from "pages/Dashboard";
 
+const API_URL = "https://api.fake-rest.refine.dev";
 function App() {
   return (
-    <ThemeProvider theme={LightTheme}>
-      <CssBaseline />
-      <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
-      <Dashboard />
-    </ThemeProvider>
+    <Refine
+        routerProvider={routerProvider}
+        resources={[ { name: "dashboard", list: Dashboard} ]}
+        dataProvider={dataProvider(API_URL)}
+        DashboardPage={Dashboard}
+    />
   );
 }
 
